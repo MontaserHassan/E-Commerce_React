c800f81f4acaa6f2d6565499988d5f1870432e6a
Amani khaled <Amani.khaled.mohamed@gmail.com>
2023-05-17 17:22:51 +0300

    change style

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,14 +1,21 @@
 import React, { Fragment } from 'react';
 import Carousel from './Carousel/Carousel';
 import HomeFooter from './home-footer';
-
+import main from '../images/main.png'
+import './style/home.css'
 const Home = () => {
     return (
 
         <Fragment>
+            <div className="main ">
+                <img src={main} className=" w-50 " title="Clothings" alt="Clothings" />
+            </div>
             <div className="container-fluid ">
+
                 <div className="row ">
+
                     <div className="hero  mt-1 text-center ">
+
                         <Carousel />
                     </div>
                 </div>
--- src/components/home/wishlists.jsx
+++ src/components/home/wishlists.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Wishlists = () => {
     return (
-        
+
         <>
-            <h1 className="justify-content-center text-center">My Wishlist</h1>    
+            <h1 className="justify-content-center text-center">My Wishlist</h1>
         </>
-    
+
     );
 }
 
